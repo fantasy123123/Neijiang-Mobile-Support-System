9cc1840fb608fe47e5e0bd834b58c34f536d175e
xdk <1455045838@qq.com>
2024-07-12 15:00:38 +0800

    商户管理群组

--- src/Pages/ShopkeeperPage/components/ShopManageGroup.js
+++ src/Pages/ShopkeeperPage/components/ShopManageGroup.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
-import { Button, Input, Message, Table, Modal, Form } from '@arco-design/web-react';
-import { IconSearch } from '@arco-design/web-react/icon';
+import { Button, Input, Message, Table, Modal, Form, Upload, Avatar } from '@arco-design/web-react';
+import { IconSearch, IconEdit, IconDelete } from '@arco-design/web-react/icon';
 import axiosInstance from '../../../api/AxiosApi';
 
 const ShopManageGroup = () => {
@@ -8,9 +8,12 @@ const ShopManageGroup = () => {
     const [memberData, setMemberData] = useState([]);
     const [visible, setVisible] = useState(false);
     const [addMemberVisible, setAddMemberVisible] = useState(false);
+    const [editGroupVisible, setEditGroupVisible] = useState(false);
     const [currentGroupId, setCurrentGroupId] = useState(null);
     const [form] = Form.useForm();
     const [addMemberForm] = Form.useForm();
+    const [editGroupForm] = Form.useForm();
+
 
     useEffect(() => {
         fetchGroups();
@@ -18,7 +21,7 @@ const ShopManageGroup = () => {
 
     const fetchGroups = async () => {
         try {
-            const response = await axiosInstance.get('/groups');
+            const response = await axiosInstance.get('/groups/accounts/' + localStorage.getItem('accountId'));
             let tempGroup = response.data.data.map(value => ({ ...value, key: value.groupId }));
             tempGroup.forEach((group, index) => {
                 tempGroup[index].createdAt = formatDate(group.createdAt);
@@ -44,9 +47,16 @@ const ShopManageGroup = () => {
             title: '群组编号',
             dataIndex: 'groupId',
             sorter: (a, b) => a.groupId - b.groupId,
+            align: 'center',
         },
         {
-            title: '群组名称',
+            title: '头像',
+            dataIndex: 'imageUrl',
+            render: (imageUrl) => <img src={imageUrl} alt="group" style={{ width: 50, height: 50 }} />,
+            align: 'center',
+        },
+        {
+            title: '名称',
             dataIndex: 'groupName',
             filterIcon: <IconSearch />,
             filterDropdown: ({ filterKeys, setFilterKeys, confirm }) => {
@@ -73,20 +83,23 @@ const ShopManageGroup = () => {
                     setTimeout(() => inputRef1.current.focus(), 150);
                 }
             },
+            align: 'center',
         },
         {
             title: '描述',
-            dataIndex: 'description'
+            dataIndex: 'description',
+            align: 'center',
         },
         {
             title: '创建时间',
             dataIndex: 'createdAt',
             sorter: (a, b) => a.createdAt - b.createdAt,
+            align: 'center',
         },
         {
             title: '操作',
             render: (col, record) => {
-                return <div style={{ display: 'flex' }}>
+                return <div style={{ display: 'flex', justifyContent: 'center' }}>
                     <Button
                         style={{ marginLeft: 10 }}
                         type="primary"
@@ -98,7 +111,18 @@ const ShopManageGroup = () => {
                         添加成员
                     </Button>
                     <Button
-                        style={{width:'87.6px', marginLeft: 20}}
+                        style={{ width: '87.6px', marginLeft: 20 }}
+                        type="primary"
+                        onClick={() => {
+                            setCurrentGroupId(record.groupId);
+                            setEditGroupVisible(true);
+                            editGroupForm.setFieldsValue(record);
+                        }}
+                    >
+                        修改
+                    </Button>
+                    <Button
+                        style={{ width: '87.6px', marginLeft: 20 }}
                         status={'danger'}
                         type={"primary"}
                         onClick={() => {
@@ -118,7 +142,8 @@ const ShopManageGroup = () => {
                         注销
                     </Button>
                 </div>
-            }
+            },
+            align: 'center',
         }
     ];
 
@@ -127,6 +152,13 @@ const ShopManageGroup = () => {
             title: '成员账号',
             dataIndex: 'accountId',
             sorter: (a, b) => a.accountId - b.accountId,
+            align: 'center',
+        },
+        {
+            title: '头像',
+            dataIndex: 'imageUrl',
+            render: (imageUrl) => <img src={imageUrl} alt="member" style={{ width: 50, height: 50 }} />,
+            align: 'center',
         },
         {
             title: '成员名称',
@@ -156,21 +188,34 @@ const ShopManageGroup = () => {
                     setTimeout(() => inputRef2.current.focus(), 150);
                 }
             },
+            align: 'center',
         },
         {
             title: '成员角色',
             dataIndex: 'role',
+            align: 'center',
         },
         {
-            title: '注册时间',
-            dataIndex: 'createdAt',
-            sorter: (a, b) => a.createdAt - b.createdAt,
-        },
+            title: '操作',
+            render: (col, record) => {
+                return <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    {record.role !== '管理员' && (
+                        <Button
+                            type="primary"
+                            status="danger"
+                            icon={<IconDelete />}
+                            onClick={() => handleDeleteMember(record.groupId, record.accountId)}
+                        />
+                    )}
+                </div>
+            },
+            align: 'center',
+        }
     ];
 
     const formatDate = (dateString) => {
         const date = new Date(dateString);
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZoneName: 'short' };
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
         return date.toLocaleDateString('en-US', options);
     };
 
@@ -194,6 +239,7 @@ const ShopManageGroup = () => {
     const handleOk = async () => {
         try {
             const values = await form.validate();
+            values.ownerId = localStorage.getItem("accountId");
             const response = await axiosInstance.post('/groups', values);
             if (response.data.status === 'success') {
                 Message.success('群组创建成功');
@@ -219,7 +265,7 @@ const ShopManageGroup = () => {
             const response = await axiosInstance.post('/groups/members', {
                 groupId: currentGroupId,
                 accountId: values.accountId,
-                role: values.role
+                role: "成员"
             });
             if (response.data.status === 'success') {
                 Message.success('成员添加成功');
@@ -235,6 +281,74 @@ const ShopManageGroup = () => {
         }
     };
 
+    const handleEditGroupCancel = () => {
+        setEditGroupVisible(false);
+    };
+
+    const handleEditGroupOk = async () => {
+        try {
+            const values = await editGroupForm.validate();
+            values.groupId = currentGroupId;
+            const {imageUrl,...rest}=values;
+            const response = await axiosInstance.put(`/groups`, rest);
+            if (response.data.status === 'success') {
+                Message.success('群组信息修改成功');
+                setEditGroupVisible(false);
+                editGroupForm.resetFields();
+                fetchGroups(); // 重新获取群组数据
+            } else {
+                Message.error('群组信息修改失败');
+            }
+        } catch (error) {
+            console.error(error);
+            Message.error('群组信息修改失败');
+        }
+    };
+
+    const handleImageUpload = async (file, fileList) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        try {
+            const response = await axiosInstance.post(`/groups/images/${currentGroupId}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (response.data.status === 'success') {
+                Message.success('头像上传成功');
+                editGroupForm.setFieldsValue({ imageUrl: response.data.message });
+                fileList.push(file)
+                fetchGroups(); // 重新获取群组数据
+            } else {
+                Message.error('头像上传失败');
+            }
+        } catch (error) {
+            console.error(error);
+            Message.error('头像上传失败');
+        }
+    };
+
+    const handleDeleteMember = async (groupId, accountId) => {
+        try {
+            const response = await axiosInstance.delete('/groups/members', {
+                data: {
+                    groupId: groupId,
+                    accountId: accountId
+                }
+            });
+            if (response.data.status === 'success') {
+                Message.success('成员删除成功');
+                fetchGroups(); // 重新获取群组数据
+            } else {
+                Message.error('成员删除失败');
+            }
+        } catch (error) {
+            console.error(error);
+            Message.error('成员删除失败');
+        }
+    };
+
+
     return (
         <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div style={{ width: "90%", height: '90%' }}>
@@ -243,7 +357,6 @@ const ShopManageGroup = () => {
                 </div>
 
                 <Modal
-                    unmountOnExit={true}
                     title="创建群组"
                     visible={visible}
                     onOk={handleOk}
@@ -252,16 +365,16 @@ const ShopManageGroup = () => {
                     focusLock={true}
                 >
                     <Form form={form} layout="vertical">
-                        <Form.Item label="群组名称" field="groupName" rules={[{ required: true, message: '请输入群组名称' }]}>
-                            <Input placeholder="请输入群组名称" />
+                        <Form.Item label="名称" field="groupName" rules={[{ required: true, message: '请输入名称' }]}>
+                            <Input placeholder="请输入名称" />
                         </Form.Item>
                         <Form.Item label="描述" field="description">
                             <Input.TextArea placeholder="请输入群组描述" />
                         </Form.Item>
+                        {/* 创建群组时不上传图片 */}
                     </Form>
                 </Modal>
                 <Modal
-                    unmountOnExit={true}
                     title="添加成员"
                     visible={addMemberVisible}
                     onOk={handleAddMemberOk}
@@ -273,8 +386,36 @@ const ShopManageGroup = () => {
                         <Form.Item label="成员账号" field="accountId" rules={[{ required: true, message: '请输入成员账号' }]}>
                             <Input placeholder="请输入成员账号" />
                         </Form.Item>
-                        <Form.Item label="成员角色" field="role" rules={[{ required: true, message: '请输入成员角色' }]}>
-                            <Input placeholder="请输入成员角色" />
+                    </Form>
+                </Modal>
+                <Modal
+                    title="修改群组信息"
+                    visible={editGroupVisible}
+                    onOk={handleEditGroupOk}
+                    onCancel={handleEditGroupCancel}
+                    autoFocus={false}
+                    focusLock={true}
+                >
+                    <Form form={editGroupForm} layout="vertical">
+                        <Form.Item label="名称" field="groupName" rules={[{ required: true, message: '请输入名称' }]}>
+                            <Input placeholder="请输入名称" />
+                        </Form.Item>
+                        <Form.Item label="描述" field="description">
+                            <Input.TextArea placeholder="请输入群组描述" />
+                        </Form.Item>
+                        <Form.Item label="头像" field="imageUrl">
+                            <Upload
+                                imagePreview={true}
+                                limit={1}
+                                multiple
+                                listType="picture-list"
+                                beforeUpload={handleImageUpload}
+                                showUploadList={true}
+                            >
+                                <Avatar>
+                                    <img src={editGroupForm.getFieldValue('imageUrl')} />
+                                </Avatar>
+                            </Upload>
                         </Form.Item>
                     </Form>
                 </Modal>
@@ -289,7 +430,7 @@ const ShopManageGroup = () => {
                     maxHeight: '82%',
                     overflow: 'auto'
                 }}>
-                    <div style={{marginRight: 30, marginTop: 20}}>
+                    <div style={{ marginRight: 30, marginTop: 20 }}>
                         <Button type="primary" onClick={handleCreateGroup}>创建群组</Button>
                     </div>
                     <Table
@@ -297,9 +438,9 @@ const ShopManageGroup = () => {
                         expandedRowRender={expandedRowRender}
                         columns={groupColumns}
                         data={groupData}
-                        style={{margin: 30}}
+                        style={{ margin: 30 }}
                         borderCell={true}
-                        pagination={{pageSize: 5}}
+                        pagination={{ pageSize: 5 }}
                     />
                 </div>
             </div>
