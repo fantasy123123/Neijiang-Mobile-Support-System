a422c512f067039fb737f8e4b8ed0084d973b997
xdk <1455045838@qq.com>
2024-07-12 14:40:41 +0800

    优化聊天室

--- src/Pages/Components/ChatRoom.js
+++ src/Pages/Components/ChatRoom.js
@@ -3,7 +3,7 @@ import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import { Client } from '@stomp/stompjs';
 import SockJS from 'sockjs-client';
 import axiosInstance from "../../api/AxiosApi";
-import { Card, List, Input, Button, Typography, Avatar } from "@arco-design/web-react";
+import { List, Input, Button, Typography, Avatar,Notification } from "@arco-design/web-react";
 import "@arco-design/web-react/dist/css/arco.css";
 import { IconArrowLeft } from "@arco-design/web-react/icon";
 
@@ -25,6 +25,9 @@ const ChatRoom = () => {
     const { groupId } = useParams();
     const groupName = location.state?.groupName;
 
+    useEffect(()=>{
+        setTimeout(scrollToBottom,10)
+    },[])
 
     useEffect(() => {
         const fetchUserInfo = async () => {
@@ -122,6 +125,7 @@ const ChatRoom = () => {
         }
     };
 
+
     const scrollToBottom = () => {
         if (listRef.current) {
             listRef.current.scrollTop = listRef.current.scrollHeight;
@@ -129,6 +133,7 @@ const ChatRoom = () => {
             setShowScrollToBottom(false);
         }
     };
+
     useEffect(() => {
         if(toBottom) {
             scrollToBottom();
@@ -152,33 +157,71 @@ const ChatRoom = () => {
         return members.find(member => member.accountId === accountId);
     };
 
+    useEffect(()=>{
+        const id = `${Date.now()}`;
+        if(showScrollToBottom){
+            Notification.info({
+                id,
+                title: '有新消息！',
+                content: '是否跳到底部？',
+                duration:5000,
+                btn: (
+                    <span>
+                    <Button
+                        type='primary'
+                        size='small'
+                        style={{ margin: '0 12px' }}
+                        onClick={()=> {
+                            scrollToBottom()
+                            Notification.remove(id)
+                        }}
+                    >
+                        跳转
+                    </Button>
+                    <Button
+                        type='secondary'
+                        size='small'
+                        onClick={()=>{Notification.remove(id)}}
+                    >
+                        否
+                    </Button>
+                </span>
+                ),
+            });
+        }
+    },[showScrollToBottom])
+
     return (
         <div style={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div style={{ width: '85%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-                <Card ref={listRef} style={{
-                    width: "100%",
-                    height: '100%',
-                    overflow: 'auto',
-                    padding: '72px 16px 72px 16px',
-                    border: '1px solid #e8e8e8'
+            <div style={{ position:'relative',width: '80%', height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                <div style={{
+                    position: 'absolute',
+                    top: 0,
+                    height:'10%',
+                    width: '100%',
+                    paddingLeft: '16px',
+                    paddingRight: '16px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    backgroundColor: '#ffffff',
+                    borderBottom: '1px solid #e8e8e8',
+                    zIndex: 2
                 }}>
-                    <div style={{
-                        position: 'fixed',
-                        top: 0,
-                        width: '71%',
-                        padding: '16px',
-                        display: 'flex',
-                        alignItems: 'center',
-                        backgroundColor: '#ffffff',
-                        borderBottom: '1px solid #e8e8e8',
-                        zIndex: 2
-                    }}>
-                        <Button type="text" onClick={() => navigate(-1)} style={{marginRight: 8}}>
-                            <IconArrowLeft style={{fontSize: '30px'}}/>
-                        </Button>
-                        <Title level={2} style={{margin: 0}}>{groupName}</Title>
-                    </div>
-                    <div style={{ overflowY: 'auto', height: 'calc(100% - 100px)' }}>
+                    <Button type="text" onClick={() => navigate(-1)} style={{marginRight: 8}}>
+                        <IconArrowLeft style={{fontSize: '30px'}}/>
+                    </Button>
+                    <Title level={2} style={{margin: 0}}>{groupName}</Title>
+                </div>
+                <div ref={listRef} style={{
+                    position: 'absolute',
+                    top: '10%',
+                    width: "100%",
+                    height: '80%',
+                    overflow: 'auto',
+                    padding: '0px 16px 10px 16px',
+                    border: '1px solid #e8e8e8',
+                    backgroundColor:'whitesmoke'
+                }}>
                         <List
                             bordered={false}
                             split={false}
@@ -190,13 +233,13 @@ const ChatRoom = () => {
                                         <List.Item key={index} style={{
                                             display: 'flex',
                                             justifyContent: 'center',
-                                            padding: '4px 0'
+                                            padding: '10px 0 5px 0'
                                         }}>
                                             <div style={{
                                                 color: '#888',
                                                 textAlign: 'center'
                                             }}>
-                                                {message.details}
+                                                {memberInfo?.name} {message.details}
                                             </div>
                                         </List.Item>
                                     );
@@ -205,7 +248,7 @@ const ChatRoom = () => {
                                         <List.Item key={index} style={{
                                             display: 'flex',
                                             justifyContent: 'center',
-                                            padding: '4px 0'
+                                            padding: '5px 0'
                                         }}>
                                             <div style={{
                                                 color: '#888',
@@ -220,7 +263,7 @@ const ChatRoom = () => {
                                         <List.Item key={index} style={{
                                             display: 'flex',
                                             justifyContent: message.accountId === username ? 'flex-end' : 'flex-start',
-                                            padding: '4px 0'
+                                            padding: '5px 0'
                                         }}>
                                             <div style={{
                                                 display: 'flex',
@@ -228,15 +271,25 @@ const ChatRoom = () => {
                                                 alignItems: 'center'
                                             }}>
                                                 <Avatar size={32} style={{margin: '0 8px'}} src={memberInfo?.imageUrl}/>
-                                                <div style={{
-                                                    backgroundColor: message.accountId === username ? '#e6f7ff' : '#ffffff',
-                                                    borderRadius: '16px',
-                                                    padding: '8px 12px',
-                                                    maxWidth: '70%',
-                                                    wordBreak: 'break-word',
-                                                    boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
-                                                }}>
-                                                    {message.details}
+                                                <div style={{maxWidth: '70%'}}>
+                                                    <div style={{
+                                                        fontSize:12,
+                                                        color:'grey',
+                                                        textAlign:message.accountId === username ? 'right':'left'
+                                                    }}
+                                                    >
+                                                        {memberInfo?.name}
+                                                    </div>
+                                                    <div style={{
+                                                        backgroundColor: message.accountId === username ? '#e6f7ff' : '#ffffff',
+                                                        borderRadius: '16px',
+                                                        padding: '8px 12px',
+                                                        maxWidth: '100%',
+                                                        wordBreak: 'break-word',
+                                                        boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)'
+                                                    }}>
+                                                        {message.details}
+                                                    </div>
                                                 </div>
                                             </div>
                                         </List.Item>
@@ -244,19 +297,22 @@ const ChatRoom = () => {
                                 }
                             }}
                         />
-                    </div>
-                </Card>
+                </div>
                 <div style={{
-                    position: 'fixed',
+                    position: 'absolute',
                     bottom: 0,
-                    width: '72.3%',
-                    padding: '16px',
+                    height:'calc(10% - 10px)',
+                    width: '100%',
+                    paddingLeft: '16px',
+                    paddingRight: '16px',
                     display: 'flex',
                     alignItems: 'center',
                     backgroundColor: '#ffffff',
                     borderTop: '1px solid #e8e8e8'
                 }}>
-                    <Input
+                    <Input.TextArea
+                        autoSize={{minRows:1,maxRows:1}}
+                        size={'large'}
                         placeholder="Enter your message"
                         value={content}
                         onChange={(e) => setContent(e)}
@@ -264,11 +320,6 @@ const ChatRoom = () => {
                     />
                     <Button type="primary" onClick={sendMessage}>Send</Button>
                 </div>
-                { showScrollToBottom && (
-                    <Button type="primary" onClick={scrollToBottom} style={{ position: 'absolute', bottom: 80, right: 600 }}>
-                        有新消息,跳到底部
-                    </Button>
-                )}
             </div>
         </div>
     );
